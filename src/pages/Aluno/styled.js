9af472edb800771384244f8e12787570ee429ac4
Robson Soares <robson.santana.soares@gmail.com>
2021-05-12 10:12:19 -0300

    Cadastro de alunos

--- src/pages/Aluno/styled.js
+++ src/pages/Aluno/styled.js
@@ -1,3 +1,64 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragraph = styled.p``;
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  label:not(:last-child) {
+    margin-bottom: 15px;
+  }
+
+  input {
+    margin-top: 10px;
+    height: 30px;
+    font-size: 16px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+    transition: all 300ms ease-in-out;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+  position: relative;
+  margin-top: 20px;
+
+  img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    position: absolute;
+    bottom: 0;
+    color: #fff;
+    background: ${colors.primaryDarkColor};
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+`;
